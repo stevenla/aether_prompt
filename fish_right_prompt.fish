f5fa33e54c9981269f3a84575d9a74ecbd308cf2
Steven La <mrstevenla@gmail.com>
2017-01-11 18:35:02 -0800

    standard colors

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,9 +1,9 @@
 function fish_right_prompt
     set -l status_copy $status
-    set -l status_color 0fc
+    set -l status_color black
 
     if test "$status_copy" -ne 0
-        set status_color f30
+        set status_color red
     end
 
     if test "$CMD_DURATION" -gt 100
@@ -15,6 +15,6 @@ function fish_right_prompt
     else if set -l last_job_id (last_job_id -l)
          echo -sn (set_color $status_color) "%$last_job_id" (set_color normal)
     else
-         echo -sn (set_color 555) (date "+%H:%M") (set_color normal)
+         echo -sn (set_color cyan) (date "+%H:%M") (set_color normal)
     end
 end
